87ef9b73c6699dae172b284ccefcbec6cb17fea6
KeishiroNagaseForMacBookPro <keit0728.job@gmail.com>
2022-12-21 03:09:49 +0900

    upload to github

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: [
+      "images.unsplash.com", 
+      "unsplash.com", 
+      "plus.unsplash.com",
+      "ibb.co",
+      "i.ibb.co",
+    ],
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 }
 
 module.exports = nextConfig
